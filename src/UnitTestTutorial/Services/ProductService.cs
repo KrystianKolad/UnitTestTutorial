ad2575e679a3023ad4509b7948afcbb585142213
Krystian <krystiankolad@wp.pl>
2018-01-05 23:46:09 +0100

    Added fields

--- src/UnitTestTutorial/Services/ProductService.cs
+++ src/UnitTestTutorial/Services/ProductService.cs
@@ -1,9 +1,21 @@
+using UnitTestTutorial.Helpers.Interfaces;
+using UnitTestTutorial.Model;
+using UnitTestTutorial.ReadRepository.Interfaces;
 using UnitTestTutorial.Services.Interfaces;
+using UnitTestTutorial.WriteRepository.Interfaces;
 
 namespace UnitTestTutorial.Services
 {
     public class ProductService : IProductService
     {
-        
+        private IDiscountHelper _discountHelper;
+        private IWriteRepository<Product> _productWriteRepository;
+        private IReadRepository<Product> _productReadRepository;
+        public ProductService(IDiscountHelper discountHelper, IWriteRepository<Product> productWriteRepository, IReadRepository<Product> productReadRepository)
+        {
+            _discountHelper = discountHelper ?? throw new System.ArgumentNullException(nameof(discountHelper));
+            _productWriteRepository = productWriteRepository ?? throw new System.ArgumentNullException(nameof(productWriteRepository));
+            _productReadRepository = productReadRepository ?? throw new System.ArgumentNullException(nameof(productReadRepository));
+        }
     }
 }
--- src/UnitTestTutorial/Services/UserService.cs
+++ src/UnitTestTutorial/Services/UserService.cs
@@ -1,9 +1,18 @@
+using UnitTestTutorial.Model;
+using UnitTestTutorial.ReadRepository.Interfaces;
 using UnitTestTutorial.Services.Interfaces;
+using UnitTestTutorial.WriteRepository.Interfaces;
 
 namespace UnitTestTutorial.Services
 {
     public class UserService : IUserService
     {
-        
+        private IWriteRepository<User> _userWriteRepository;
+        private IReadRepository<User> _userReadRepository;
+        public UserService(IWriteRepository<User> userWriteRepository, IReadRepository<User> userReadRepository)
+        {
+            _userWriteRepository = userWriteRepository ?? throw new System.ArgumentNullException(nameof(userWriteRepository));
+            _userReadRepository = userReadRepository ?? throw new System.ArgumentNullException(nameof(userReadRepository));
+        }
     }
 }
