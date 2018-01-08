56718f2e08c947a6b8da0d0c723e279e9e82aa51
Krystian <krystiankolad@wp.pl>
2018-01-08 16:53:13 +0100

    Last tests

--- tests/UnitTestTutorial.Test/Services/ProductServiceTests.cs
+++ tests/UnitTestTutorial.Test/Services/ProductServiceTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Moq;
 using NUnit.Framework;
 using UnitTestTutorial.Helpers.Interfaces;
@@ -29,6 +30,54 @@ namespace UnitTestTutorial.Test.Services
 
             _sut = new ProductService(_discountHelperMock.Object,_productWriteRepositoryMock.Object,_productReadRepositoryMock.Object);
         }
+
+        [Test]
+        public void AddProduct_Should_Call_Add_From_Repository()
+        {
+            Product product = new Product();
+
+            _sut.AddProduct(product);
+
+            _productWriteRepositoryMock.Verify(x=>x.Add(It.IsAny<Product>()));
+        }
+
+        [Test]
+        public void GetSortedProducts_Should_GetProducts_Sorted_By_Price()
+        {
+            IList<Product> products = new List<Product>(){
+                new Product(){Price = 5},
+                new Product(){Price = 3},
+                new Product(){ Price = 8}
+            };
+
+            _productReadRepositoryMock.Setup(x=>x.GetAll()).Returns(products);
+
+            var result = _sut.GetSortedProducts();
+
+            Assert.IsNotEmpty(result);
+            Assert.AreEqual(3,result[0].Price);
+            Assert.AreEqual(5,result[1].Price);
+            Assert.AreEqual(8,result[2].Price);
+        }
         
+        [Test]
+        public void GetProductsPrice_Should_Return_Valid_Price()
+        {
+            IList<Product> products = new List<Product>(){
+                new Product(){Price = 5},
+                new Product(){Price = 3},
+                new Product(){ Price = 8}
+            };
+
+            User user = new User();
+
+            decimal expectedResult = 16;
+
+            _discountHelperMock.Setup(x=>x.GetDiscount(It.IsAny<User>(),16)).Returns(16);
+
+            decimal actualResult = _sut.GetProductsPrice(user,products);
+
+            Assert.AreEqual(expectedResult,actualResult);
+        }
     }
 }
--- tests/UnitTestTutorial.Test/Services/UserServiceTests.cs
+++ tests/UnitTestTutorial.Test/Services/UserServiceTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Moq;
 using NUnit.Framework;
 using UnitTestTutorial.Model;
@@ -25,6 +26,36 @@ namespace UnitTestTutorial.Test.Services
             _userWriteRepositoryMock = new Mock<IWriteRepository<User>>();
 
             _sut = new UserService(_userWriteRepositoryMock.Object,_userReadRepositoryMock.Object);
+        }
+
+        [Test]
+        public void AddUser_Should_Call_Add_From_Repository()
+        {
+            User user = new User();
+
+            _sut.AddUser(user);
+
+            _userWriteRepositoryMock.Verify(x=>x.Add(It.IsAny<User>()));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        public void GetUsersByType_Should_GetUsers_With_Only_That_Type(int userType)
+        {
+            IList<User> users = new List<User>(){
+                new User(){UserType = 1},
+                new User(){UserType = 2},
+                new User(){UserType = 0}
+            };
+
+            _userReadRepositoryMock.Setup(x=>x.GetAll()).Returns(users);
+
+            var result = _sut.GetUsersByType(userType);
+
+            Assert.IsNotEmpty(result);
+            Assert.AreEqual(userType,result[0].UserType);
         }
     }
 }
